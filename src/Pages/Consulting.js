f2394f680c6b668920e29c21431d980b6c82721c
IM123EN <88934418+IM123EN@users.noreply.github.com>
2021-11-13 16:07:31 +0100

    Add files via upload

---
+++ src/Pages/Consulting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Consulting() {
+    return (
+        <div>
+         <h1  className="consulting"> Par Région </h1>  
+        </div>
+    )
+}
+
+export default Consulting
